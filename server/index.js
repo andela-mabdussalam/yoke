04f363ca6a3af96bbb23ef6686c23d9e067f757c
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-04-18 18:06:58 +0100

    chore(directory structure): modify directory structure

--- server/index.js
+++ server/index.js
@@ -4,18 +4,19 @@ import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
-import WebpackConfig from '../webpack.config.dev.js';
-let app = express();
+import WebpackConfig from '../webpack.config.dev';
+
+const app = express();
 const compiler = webpack(WebpackConfig);
 app.use(webpackMiddleware(compiler, {
   hot: true,
-  publicPath:  WebpackConfig.output.publicPath,
+  publicPath: WebpackConfig.output.publicPath,
   noInfo: true
 }));
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.listen(3000, () => console.log('Running on localhost:3000'));
