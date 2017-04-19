f20ddbb7554c76899496e7cd382f62f2429ec9d0
mariam <mariam.abdussalam@andela.com>
2017-04-19 13:30:15 +0100

    add webpack resolvers

--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => (
-    <nav className="navbar navbar-toggleable-md navbar-light navbar-custom">
+  <nav className="navbar navbar-toggleable-md navbar-light navbar-custom">
     <button className="navbar-toggler navbar-toggler-right" type="button"
       data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false"
       aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <Link to="/"><a className="navbar-brand" href="#">YOKE</a></Link>
+      <span className="navbar-toggler-icon" />
+    </button>
+    <Link to="/"><a className="navbar-brand" href="#">YOKE</a></Link>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
           <Link to="/"><a className="nav-link" href="#">Home
                <span className="sr-only">(current)</span></a></Link>
-          </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
+        </li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
         <li className="extra"><Link to="/signup">
-          <i className="fa fa-user-o icon" aria-hidden="true">
-          </i>Sign Up</Link></li>
+          <i className="fa fa-user-o icon" aria-hidden="true" />
+          Sign Up</Link></li>
         <li><a href="#"><i className="fa fa-sign-in icon"
-          aria-hidden="true"></i>Login</a></li>
-        </ul>
-      </div>
-    </nav>
-  );
+          aria-hidden="true"/>Login</a></li>
+      </ul>
+    </div>
+  </nav>
+);
